a1c5e89fcbf51f469ae77cd9ef9181c60761a217
David Ciani <dciani@davidciani.com>
2021-04-04 22:49:39 -0700

    revised cash-donations

--- cash-donations.py
+++ cash-donations.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
+import argparse
+import logging
 from csv import DictReader
+from datetime import date, datetime
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
 
 header = """V042
-A TaxTool Donations 0.3
-D 04/13/2019
+ATaxTool Donations 0.3
+D{date:%Y-%m-%d}
 ^
 """
 
@@ -13,7 +19,7 @@ N280
 C1
 L1
 ${amount:0.2f}
-X {payee} ({ein})
+X{payee} ({ein})
 ^
 """
 
@@ -27,29 +33,48 @@ X {payee}/{note} ({ein})
 """
 
 
-def main():
-    print(header)
-    with open("cash-charity-2018.csv") as f:
-        rdr = DictReader(f)
-        for record in rdr:
-            if record["Notes"] is not "":
-                print(
-                    record_layout_2.format(
-                        payee=record["Payee"],
-                        ein=record["EIN"],
-                        note=record["Notes"],
-                        amount=-float(record["2018"]),
+def main(args):
+
+    input_file = Path(args.input).resolve(strict=True)
+    logger.info(f"Input file: {input_file}")
+
+    if args.output is None:
+        output_file = input_file.with_stem(
+            input_file.stem + f"_{datetime.now():%Y%m%d_%H%M%S}"
+        ).with_suffix(".txf")
+    else:
+        output_file = Path(args.output).resolve()
+    logger.info(f"Output file: {output_file}")
+
+    with input_file.open("r") as f:
+        records = list(DictReader(f))
+
+    with output_file.open("x", newline="\r\n") as f:
+        f.write(header.format(date=date.today()))
+        for record in records:
+            if float(record["amount"]) > 0:
+                if record["notes"] != "":
+                    f.write(
+                        record_layout_2.format(
+                            payee=record["payee"],
+                            ein=record["ein"],
+                            note=record["notes"],
+                            amount=-float(record["amount"]),
+                        )
                     )
-                )
-            else:
-                print(
-                    record_layout_1.format(
-                        payee=record["Payee"],
-                        ein=record["EIN"],
-                        amount=-float(record["2018"]),
+                else:
+                    f.write(
+                        record_layout_1.format(
+                            payee=record["payee"],
+                            ein=record["ein"],
+                            amount=-float(record["amount"]),
+                        )
                     )
-                )
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="Input CSV file")
+    parser.add_argument("-o", "--output", help="File to write TXF output to")
+
+    main(parser.parse_args())
